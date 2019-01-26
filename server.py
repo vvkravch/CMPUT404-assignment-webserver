d16bfa7d7d2b3191b9441b87e5934d3507d80d94
Vlad Kravchenko <vvkravch@ualberta.ca>
2019-01-25 19:21:00 -0500

    Css type

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 #  coding: utf-8 
 import socketserver
+import cgitb
+cgitb.enable()
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -25,6 +27,30 @@ import socketserver
 # run: python freetests.py
 
 # try: curl -v -X GET http://127.0.0.1:8080/
+import os
+
+
+def directoryContents ():
+    #use www from current direcotry
+    path = "./www"
+    contents=os.listdir(path)
+    
+    return contents
+def makehtml():
+    directoryhtml="<!DOCTYPE html> <html> <head> <title>Page Title</title> </head> <body> <h1>My First Heading</h1> <p>My first paragraph. </p> </body>\n</html\>"
+    return directoryhtml
+def SendFile(filepath):
+    filepath2="./www"+filepath
+    file = open(filepath2,'r')
+    if file.name[len(file.name)-1] == "s":
+        http_response = """HTTP/1.1 200 OK"""+"""<style Content-Type="text/css)"""+"""\r\n"""+ file.read() +"""</style>"""
+    if file.name[len(file.name)-1] == "l":
+        http_response = """HTTP/1.1 200 OK"""+"""Content-Type: text/html"""+"""\r\n""" +file.read() +""""""
+    #else: 
+     #   http_response = """HTTP/1.1 404"""
+       
+    return(http_response)
+        
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
@@ -32,7 +58,24 @@ class MyWebServer(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        splitdata=str(self.data).split()
+        print(splitdata[1])
+    
+        #headers = mimetools.Message(StringIO.StringIO
+        
+        if splitdata[1] == "/do-not-implement-this-page-it-is-not-found":
+
+            self.request.sendall(bytearray(("""HTTP/1.1 404"""),'utf-8'))        
+        if splitdata[1] == "/":
+            response="""HTTP/1.1 200 OK """
+            self.request.sendall(bytearray((response),'utf-8'))
+            
+        elif splitdata[1] == "/index.html" or "/base.css" or "/deep/index.html":
+   
+            self.request.sendall(bytearray(SendFile(splitdata[1]),'utf-8'))
+            
+       
+        
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
